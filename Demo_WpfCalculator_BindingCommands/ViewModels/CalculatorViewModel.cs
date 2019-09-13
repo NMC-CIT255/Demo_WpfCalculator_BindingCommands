e4ea3f04e3f53b12cfd564887362bc7ef5c35a59
johnvelis <john@johnvelis.com>
2019-09-13 14:54:47 -0400

    Add project files.

---
+++ Demo_WpfCalculator_BindingCommands/ViewModels/CalculatorViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Demo_WpfCalculator_BindingCommands;
+
+namespace Demo_WpfCalculator_BindingCommands.ViewModels
+{
+    public class CalculatorViewModel : ObservableObject
+    {
+        private ICommand _button1Command;
+
+        public ICommand Button1Command
+        {
+            get
+            {
+                return _button1Command;
+            }
+            set
+            {
+                _button1Command = value;
+            }
+        }
+
+
+        private string _displayContent;
+
+        public string DisplayContent
+        {
+            get { return _displayContent; }
+            set
+            {
+                _displayContent = value;
+                OnPropertyChanged("DisplayContent");
+            }
+        }
+
+        public CalculatorViewModel()
+        {
+            _displayContent = "9999";
+            Button1Command = new RelayCommand(new Action<object>(ChangeScreen));
+        }
+
+        private void ChangeScreen(object obj)
+        {
+            DisplayContent = "Hello John!!";
+        }
+    }
+}
